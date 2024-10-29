e264e601ba356395186d971462f99ede7184a357
August Rouvari <august.rouvari@helsinki.fi>
2024-10-29 21:22:22 +0200

    lisätty testejä

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -15,26 +15,50 @@ class TestVarasto(unittest.TestCase):
 
     def test_lisays_lisaa_saldoa(self):
         self.varasto.lisaa_varastoon(8)
-
         self.assertAlmostEqual(self.varasto.saldo, 8)
 
     def test_lisays_lisaa_pienentaa_vapaata_tilaa(self):
         self.varasto.lisaa_varastoon(8)
-
         # vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 2)
 
     def test_ottaminen_palauttaa_oikean_maaran(self):
         self.varasto.lisaa_varastoon(8)
-
         saatu_maara = self.varasto.ota_varastosta(2)
-
         self.assertAlmostEqual(saatu_maara, 2)
 
     def test_ottaminen_lisaa_tilaa(self):
         self.varasto.lisaa_varastoon(8)
-
         self.varasto.ota_varastosta(2)
-
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_liikaa_tavaraa(self):
+        self.varasto.lisaa_varastoon(100)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 0)
+
+    def test_otetaan_liikaa(self):
+        self.varasto.lisaa_varastoon(10)
+        saatu_maara = self.varasto.ota_varastosta(100)
+        self.assertAlmostEqual(saatu_maara, 10)
+    
+    def test_lisataan_nolla(self):
+        self.varasto.lisaa_varastoon(0)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+
+    def test_lisataan_negatiivinen(self):
+        self.varasto.lisaa_varastoon(-1)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+    
+    def test_negatiivinen_saldo(self):
+        varasto = Varasto(10, -1)
+        self.assertAlmostEqual(varasto.saldo, 0)
+    
+    def test_tilavuus_negatiivinen(self):
+        varasto = Varasto(-10, 0)
+        self.assertAlmostEqual(varasto.tilavuus, 0)
+    
+    def test_otetaan_negatiivinen(self):
+        saatu_maara = self.varasto.ota_varastosta(-10)
+        self.assertAlmostEqual(saatu_maara, 0)
+    
